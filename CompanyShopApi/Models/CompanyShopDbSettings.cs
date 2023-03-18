e44e04f7f68e83c633df2f415a2e335fbe38e919
Александр Баскаков <basc11111@gmail.com>
2023-03-18 19:31:18 +0300

    add config model

---
+++ CompanyShopApi/Models/CompanyShopDbSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace CompanyShopApi.Models;
+
+public class CompanyShopDbSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string CategoryCollectionName { get; set; } = null!;
+}
--- CompanyShopApi/Program.cs
+++ CompanyShopApi/Program.cs
@@ -1,7 +1,10 @@
+using CompanyShopApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.Configure<CompanyShopDbSettings>(
+    builder.Configuration.GetSection("CompanyShopMongoDb"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
